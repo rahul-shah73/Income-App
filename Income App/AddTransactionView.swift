e8b05aa2395227c1e0fc80b0d28dcda6f7bda890
DakuGabbar73 <rahulshah887859@gmail.com>
2025-05-18 13:59:23 +0530

    FloatingButton Added with navigation Stack

---
+++ Income App/AddTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTransactionView.swift
+//  Income App
+//
+//  Created by Rahul Shah on 18/05/25.
+//
+
+import SwiftUI
+
+struct AddTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTransactionView()
+}
