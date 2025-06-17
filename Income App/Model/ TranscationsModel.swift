22c6ea63d8aeba3c433ba32e07dcae8cebb2429c
DakuGabbar73 <rahulshah887859@gmail.com>
2025-06-17 22:43:56 +0530

    Create a SwiftData In the codebase

--- Income App/Model/ TranscationsModel.swift
+++ Income App/Model/ TranscationsModel.swift
@@ -7,7 +7,42 @@
 
 import Foundation
 import SwiftUI
+import SwiftData
 
+@Model class TransactionModel {
+    var id : UUID
+    var name : String
+    var  type : TransactionType
+    var date : Date
+    var amount : Double
+    
+    init(id : UUID, name: String, type: TransactionType, date: Date, amount: Double){
+        
+        self.id = UUID()
+        self.name = name
+        self.type = type
+        self.date = date
+        self.amount = amount
+
+    }
+    
+    var displayDate : String {
+     let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        return dateFormatter.string(from: date)
+        
+    }
+    
+   
+    
+    func displayAmount(currency : Currency) -> String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = currency.locale
+        return numberFormatter.string(from: NSNumber(value: amount)) ?? ""
+    }
+    
+}
 
 struct TranscationsModel : Identifiable, Hashable {
     let id =  UUID()
